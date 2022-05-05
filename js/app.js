4c31c76f622117dbd8fe9694c68c624cc24c8c48
Alejandro Sánchez <alejandro10m_568@hotmail.com>
2022-05-05 10:17:01 -0500

    Add function to delete a determinate expense

--- js/app.js
+++ js/app.js
@@ -35,6 +35,11 @@ class Presupuesto{
         const gastado = this.gastos.reduce( (total, gasto) => total + gasto.cantidad, 0);
         this.restante = this.presupuesto - gastado;
     }
+
+    eliminarGasto(id){
+        this.gastos = this.gastos.filter( gasto => gasto.id !== id );
+        this.calcularRestante();
+    }
 }
 
 class UI{
@@ -68,7 +73,7 @@ class UI{
         }, 3000);
     }
 
-    agregarGastoListado(gastos){
+    mostrarGastos(gastos){
         this.limpiarHTML(); // Limpiar el HTML previo
         
         // Iterar sobre los gastos
@@ -89,6 +94,9 @@ class UI{
             const btnBorrar = document.createElement('button');
             btnBorrar.classList.add('btn', 'btn-danger', 'borrar-gasto');
             btnBorrar.innerHTML = 'Borrar &times;';
+            btnBorrar.onclick = () => {
+                eliminarGasto(id);
+            }
             
             nuevoGasto.appendChild(btnBorrar);
 
@@ -119,6 +127,9 @@ class UI{
         } else if( (presupuesto / 2 ) > restante ){
             restanteDiv.classList.remove('alert-success');
             restanteDiv.classList.add('alert-warning');
+        } else {
+            restanteDiv.classList.remove('alert-danger', 'alert-warning');
+            restanteDiv.classList.add('alert-success');
         }
 
         // Si el total es 0 o menor
@@ -126,6 +137,8 @@ class UI{
             ui.imprimirAlerta('El presupuesto se ha agotado', 'error');
 
             formulario.querySelector('button[type="submit"]').disabled = true;
+        } else{
+            formulario.querySelector('button[type="submit"]').disabled = false;
         }
     }
     
@@ -138,8 +151,7 @@ const ui = new UI();
 // Funciones
 
 function preguntarPresupuesto(){
-    //const presupuestoUsuario = prompt('¿Cual es tu presupuesto?');
-    const presupuestoUsuario = 500;
+    const presupuestoUsuario = prompt('¿Cual es tu presupuesto?');
 
     if(presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0){
         window.location.reload();
@@ -176,11 +188,22 @@ function agregarGasto(e){
 
     // Imprimir los gastos
     const {gastos, restante} = presupuesto;
-    ui.agregarGastoListado(gastos);
+    ui.mostrarGastos(gastos);
     ui.actualizarRestante(restante);
     ui.comprobarPresupuesto(presupuesto);
 
     // Reinicia el formulario
     formulario.reset();
 
+}
+
+function eliminarGasto(id){
+    // Elimina los gastos del objeto
+    presupuesto.eliminarGasto(id);
+
+    // Elimina los gastos del HTML
+    const {gastos, restante} = presupuesto;
+    ui.mostrarGastos(gastos);
+    ui.actualizarRestante(restante);
+    ui.comprobarPresupuesto(presupuesto);
 }
